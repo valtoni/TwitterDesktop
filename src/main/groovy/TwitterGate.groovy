4781347a9b09adfc1a61e7bf9749dc788d6e7bed
Valtoni Boaventura <valtoni@gmail.com>
2009-06-19 11:16:26 -0300

    Entrada de exemplos, mais para testes do Git

---
+++ src/main/groovy/TwitterGate.groovy
@@ -0,0 +1,5 @@
+import twitter4j.Twitter
+
+println "TwitterGate"
+
+twitter = new Twitter("javaltoni", "")status = twitter.updateStatus("Um post do groovy! Muito legal. Tempo: " + new Date())println "Status alterado para " + status.text
